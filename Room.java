413dbf4845c70642001e055293b752c0e7472d5f
OceaneL <oceane.legrand@etudiant.univ-lille1.fr>
2015-09-18 17:07:03 +0200

    Update Room.java
    Code non tester, besoin de la classe room non encore créer et nécessite une fonction doorDescription qui décrit la porte : nécessite une clé, porte normal .... si c'est une porte caché qui ne peux pas encore être vu renvoie null.

--- Room.java
+++ Room.java
@@ -1,1 +1,48 @@
+import java.util.*;
 
+/**
+ * @author legrand
+ *
+ */
+public class Room {
+	
+	String name;
+	Map<String, Door> doors;
+	boolean GameIsFinish;
+	boolean GameIsLost; 
+	
+	
+
+	public Room(String name, Map<String, Door> doors) {
+		super();
+		this.name = name;
+		this.doors = doors;
+		this.GameIsFinish = false;
+		this.GameIsLost = false;
+	}
+		
+	public Room(String name, Map<String, Door> doors, boolean gameIsFinish,
+		boolean gameIsLost) {
+		super();
+		this.name = name;
+		this.doors = doors;
+		this.GameIsFinish = gameIsFinish;
+		this.GameIsLost = gameIsLost;
+	}
+
+
+
+	public String RoomDescription() {
+		String result = "";
+		for(Map.Entry<String, Door> door : this.doors.entrySet()) {
+			String doorDescript = door.getValue().doorDescription;
+			if (doorDescript != null)
+				result += door.getKey()+" : "+doorDescript;
+		}	
+	}
+	
+	public boolean canPass(){
+		return true;
+	}
+
+}
